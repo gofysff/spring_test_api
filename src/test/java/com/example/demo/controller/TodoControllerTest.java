78e99daaa3f2e5290796d0c9fad930d62171f8ed
gofysf <toniush75@gmail.com>
2023-05-27 11:54:54 +0300

    unit tests

--- src/test/java/com/example/demo/controller/TodoControllerTest.java
+++ src/test/java/com/example/demo/controller/TodoControllerTest.java
@@ -5,12 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TodoControllerTest {
@@ -20,10 +20,11 @@ public class TodoControllerTest {
     @Autowired
     private TodoController todoController;
 
-
     private final static Long existingUserId = 2L;
 
     private final static Long nonExistingUserId=100000L;
+
+    private final static Long existingTaskId = 1L;
 
     @Test
     public void contextLoads() throws Exception{
@@ -32,14 +33,21 @@ public class TodoControllerTest {
 
     @Test
     public void shouldReturnTasksFromExistingUser() throws Exception{
-        // it can be crashed if we delete user with id 2
         String url = "/todo?todoId="+existingUserId;
-        this.mockMvc.perform(get(url)).andDo(print()).andExpectAll(status().isOk());
+        this.mockMvc.perform(get(url)).andDo(print()).andExpect(status().isOk());
 
     }
 
     @Test
-    public void shouldReturnBadRequestAfterAskingNonExistingUserTaks() throws Exception{
+    public void shouldUpdateStatusTasksFromExistingUser() throws Exception{
+
+        String url = "/todo?id="+existingTaskId;
+        this.mockMvc.perform(put(url)).andDo(print()).andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void shouldReturnBadRequestAfterAskingNonExistingUserTasks() throws Exception{
         String url = "/todo?todoId="+nonExistingUserId;
         this.mockMvc.perform(get(url)).andDo(print()).andExpect(status().isBadRequest());
     }
